d710143dc9f553d61a7998ed89c7581b12a69ea6
Taylor <laxchief@email.arizona.edu>
2015-03-07 00:47:02 -0700

    added a line

--- dressme/api.py
+++ dressme/api.py
@@ -29,7 +29,8 @@ class Rating(db.Model):
         return '<rating: %r, "%r">' % (self.score, self.comment)
 
 @app.route('/')
-
+def hello():
+	return "Hello World"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
