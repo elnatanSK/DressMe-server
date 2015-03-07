bb10b02765a89d8339b5aafbacd4d271ae85a32a
Matt Gautreau <mgaut72@gmail.com>
2015-03-07 11:26:07 -0700

    add tests for assingming ratings and stuff

--- test/test_db.py
+++ test/test_db.py
@@ -5,19 +5,66 @@ db.drop_all()
 db.create_all()
 
 u = User("matt")
+
+db.session.add(u)
+db.session.commit()
+
+o = Outfit("<image>")
+
+u.add_outfit(o)
 print u
 
-db.session.add(u)
-db.session.commit()
-
-print db.session.query(User).all()
-
-u.add_outfit("asdf")
+r = Rating(o)
+o.add_rating(r)
 
 db.session.add(u)
+db.session.add(o)
+db.session.add(r)
 db.session.commit()
 
+print db.session.query(Outfit).all()
 print db.session.query(User).all()
+print db.session.query(Rating).all()
 
-matt = db.session.query(User).get("matt")
-print matt
+print
+print
+
+u2 = User("Zach")
+
+u2.assign_rating(r)
+db.session.add(r)
+db.session.add(u2)
+db.session.commit()
+
+
+print db.session.query(Outfit).all()
+print db.session.query(User).all()
+print db.session.query(Rating).all()
+
+#print db.session.query(User).all()
+#
+#u.add_outfit("asdf")
+#
+#db.session.add(u)
+#db.session.commit()
+#
+#print db.session.query(User).all()
+#
+#matt = db.session.query(User).get("matt")
+#print matt
+#
+#o = matt.outfits[0]
+#print o
+#o.add_rating(4, "not too bad at all")
+#print o
+#
+#db.session.add(o)
+#db.session.commit()
+#print db.session.query(User).all()
+#
+#u2 = User("zach")
+#u2.add_outfit("<zachs outfit pic>")
+#db.session.add(u2)
+#db.session.commit()
+#print db.session.query(User).all()
+#
