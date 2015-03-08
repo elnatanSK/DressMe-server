d53937b8ba56dff4b6c54d4a8a037d7d069a7207
Matt Gautreau <mgaut72@gmail.com>
2015-03-08 08:10:02 -0700

    asdfasdfasdf

--- test/test_db.py
+++ test/test_db.py
@@ -57,30 +57,25 @@ print db.session.query(Rating).all()
 print db.session.query(User).get("matt")
 print db.session.query(User).get("taylor")
 
-#print db.session.query(User).all()
-#
-#u.add_outfit("asdf")
-#
-#db.session.add(u)
-#db.session.commit()
-#
-#print db.session.query(User).all()
-#
-#matt = db.session.query(User).get("matt")
-#print matt
-#
-#o = matt.outfits[0]
-#print o
-#o.add_rating(4, "not too bad at all")
-#print o
-#
-#db.session.add(o)
-#db.session.commit()
-#print db.session.query(User).all()
-#
-#u2 = User("zach")
-#u2.add_outfit("<zachs outfit pic>")
-#db.session.add(u2)
-#db.session.commit()
-#print db.session.query(User).all()
-#
+print db.session.query(User).all()
+
+db.session.add(u)
+db.session.commit()
+
+print db.session.query(User).all()
+
+matt = db.session.query(User).get("matt")
+print matt
+
+o = matt.outfits[0]
+
+db.session.add(o)
+db.session.commit()
+print db.session.query(User).all()
+
+u2 = User("zach")
+u2.add_outfit("<zachs outfit pic>")
+db.session.add(u2)
+db.session.commit()
+print db.session.query(User).all()
+
